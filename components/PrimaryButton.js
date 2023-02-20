643fe5844f51f50d89a2708dddfff6f33c647368
Clarence Lindh <clarencelindh@gmail.com>
2023-02-20 17:04:38 +0100

    Add start game screen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,7 +1,46 @@
-function PrimaryButton() {
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton({ children, onPress }) {
     return (
-        
-    )
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                onPress={onPress}
+                android_ripple={{ color: "#a01313" }} // for android
+                style={({ pressed }) =>
+                    pressed
+                        ? [styles.buttonInnerContainer, styles.pressed]
+                        : styles.buttonInnerContainer
+                }
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 30,
+        margin: 4,
+        overflow: "hidden",
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#ad1818",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2, // for android
+    },
+    buttonText: {
+        color: "#ffffff",
+        fontSize: 16,
+        textAlign: "center",
+        fontWeight: "bold",
+    },
+    pressed: {
+        // for iOs
+        opacity: 0.75,
+        backgroundColor: "#a01313",
+    },
+});
