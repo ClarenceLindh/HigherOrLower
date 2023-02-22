255329d8e7bbeb88bc69c042b3576f4bd9161a7b
cc <clarencelindh@gmail.com>
2023-02-22 17:01:45 +0100

    Fix tab size

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,46 +1,46 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children, onPress }) {
-    return (
-        <View style={styles.buttonOuterContainer}>
-            <Pressable
-                onPress={onPress}
-                android_ripple={{ color: "#a01313" }} // for android
-                style={({ pressed }) =>
-                    pressed
-                        ? [styles.buttonInnerContainer, styles.pressed]
-                        : styles.buttonInnerContainer
-                }
-            >
-                <Text style={styles.buttonText}>{children}</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={onPress}
+        android_ripple={{ color: "#a01313" }} // for android
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-    buttonOuterContainer: {
-        borderRadius: 30,
-        margin: 4,
-        overflow: "hidden",
-    },
-    buttonInnerContainer: {
-        backgroundColor: "#ad1818",
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        elevation: 2, // for android
-    },
-    buttonText: {
-        color: "#ffffff",
-        fontSize: 16,
-        textAlign: "center",
-        fontWeight: "bold",
-    },
-    pressed: {
-        // for iOs
-        opacity: 0.75,
-        backgroundColor: "#a01313",
-    },
+  buttonOuterContainer: {
+    borderRadius: 30,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#ad1818",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2, // for android
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  pressed: {
+    // for iOs
+    opacity: 0.75,
+    backgroundColor: "#a01313",
+  },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,89 +3,89 @@ import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen({ onStartGame }) {
-    const [enteredNumber, setEnteredNumber] = useState('');
+  const [enteredNumber, setEnteredNumber] = useState("");
 
-    function numberInputHandler(enteredText) {
-        setEnteredNumber(enteredText);
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      console.log("Invalid Number: ", chosenNumber);
+      Alert.alert("Invalid number!", "Number has to be between 1 and 99.", [
+        {
+          text: "Okay",
+          style: "destructive",
+          onPress: resetInputHandler,
+        },
+      ]);
+      return;
     }
+    console.log("Valid Number: ", chosenNumber);
+    onStartGame(chosenNumber);
+  }
 
-    function resetInputHandler() {
-        setEnteredNumber("");
-    }
-
-    function confirmInputHandler() {
-        const chosenNumber = parseInt(enteredNumber);
-
-        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-            console.log("Invalid Number: ", chosenNumber);
-            Alert.alert(
-                "Invalid number!",
-                "Number has to be between 1 and 99.",
-                [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
-            );
-            return;
-        }
-        console.log("Valid Number: ", chosenNumber);
-        onStartGame(chosenNumber);
-    }
-
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.numberInput}
-                maxLength={2}
-                keyboardType="number-pad"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={numberInputHandler}
-                value={enteredNumber}
-            />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={confirmInputHandler}>
-                        Confirm
-                    </PrimaryButton>
-                </View>
-            </View>
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
-    );
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 100,
-        marginHorizontal: 24,
-        borderRadius: 8,
-        padding: 16,
-        backgroundColor: "#790303",
-        elevation: 8, // for android
-        shadowColor: "black", // for iOs
-        shadowOffset: { width: 2, height: 2 }, // for iOs
-        shadowRadius: 6, // for iOs
-        shadowOpacity: 0.26, // for iOs
-    },
-    numberInput: {
-        height: 40,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: "#daad1c",
-        borderBottomWidth: 2,
-        color: "#daad1c",
-        marginVertical: 16,
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    buttonsContainer: {
-        flexDirection: "row",
-    },
-    buttonContainer: {
-        flex: 1,
-    },
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: "#790303",
+    elevation: 8, // for android
+    shadowColor: "black", // for iOs
+    shadowOffset: { width: 2, height: 2 }, // for iOs
+    shadowRadius: 6, // for iOs
+    shadowOpacity: 0.26, // for iOs
+  },
+  numberInput: {
+    height: 40,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#daad1c",
+    borderBottomWidth: 2,
+    color: "#daad1c",
+    marginVertical: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
 });
